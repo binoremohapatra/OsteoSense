8a75c1d24665d95bc2412a8ed21dbfc536c6e576
Binore Mohapatra <binoremohapatra@gmail.com>
2026-09-13 20:38:29 +0530

    Silence simulate_data import error for Pylance using type ignore

--- ai_service/wearable_model/train_lite_model.py
+++ ai_service/wearable_model/train_lite_model.py
@@ -4,10 +4,7 @@ sys.path.append(os.path.dirname(__file__))
 import json
 import numpy as np
 import tensorflow as tf
-try:
-    from simulate_data import build_dataset
-except ImportError:
-    from ai_service.wearable_model.simulate_data import build_dataset
+from simulate_data import build_dataset  # type: ignore
 
 def extract_lite_features(rec):
     # 1. normalizedPain
